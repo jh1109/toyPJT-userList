7b93db9b5dd06150fae7506ad2b5e24cd8e6db40
Juhee Noh <nohjuhee227@gmail.com>
2023-06-07 19:28:19 +0900

    learn. [5] 사용자 입력값 업데이트-상태관리(102)

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,29 @@
 // 내가 만든 UserInfoInputForm.js와 비교!
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = () => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (e) => {
     e.preventDefault();
+  };
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input type="number" id="age" />
+        <input type="number" id="age" onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
