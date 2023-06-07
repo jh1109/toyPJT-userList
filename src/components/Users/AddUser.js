aa55b3af82bb954b46eb0df46c08284d74c3ed6d
Juhee Noh <nohjuhee227@gmail.com>
2023-06-07 19:05:20 +0900

    learn. AddUser 컴포넌트 스타일링(100)

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,14 +2,14 @@
 import React from "react";
 import Card from "../UI/Card";
 
-import "./AddUser.module.css";
+import classes from "./AddUser.module.css";
 
 const AddUser = () => {
   const addUserHandler = (e) => {
     e.preventDefault();
   };
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" />
