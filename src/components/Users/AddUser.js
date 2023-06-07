18c31c873ddd570b36dab749ca0a96b31b273cdc
Juhee Noh <nohjuhee227@gmail.com>
2023-06-07 18:50:35 +0900

    try. <Card/> 컴포넌트 만들어서 디자인 적용시키기(99)

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
 // 내가 만든 UserInfoInputForm.js와 비교!
 import React from "react";
+import Card from "../UI/Card";
+
+import "./AddUser.module.css";
 
 const AddUser = () => {
   const addUserHandler = (e) => {
     e.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" />
-      <label htmlFor="age">Age (Years)</label>
-      <input type="number" id="age" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age (Years)</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
