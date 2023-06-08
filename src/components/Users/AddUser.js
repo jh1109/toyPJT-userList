ecd99704e1fd3effc862b65d12b9f1fd97734a45
Juhee Noh <nohjuhee227@gmail.com>
2023-06-08 22:11:32 +0900

    learn. 모달 컴포넌트 생성(106)

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = ({ onAddUser }) => {
   const [enteredUsername, setEnteredUsername] = useState("");
@@ -25,25 +26,28 @@ const AddUser = ({ onAddUser }) => {
     setEnteredAge(e.target.value);
   };
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
-        />
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          type="number"
-          id="age"
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            type="number"
+            id="age"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
