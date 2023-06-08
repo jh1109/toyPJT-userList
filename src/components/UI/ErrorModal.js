083a10040754b2efb45d56a33d0fbdf412e89d98
Juhee Noh <nohjuhee227@gmail.com>
2023-06-09 02:07:26 +0900

    learn. Portal 이용하여 모달창 렌더링 위치 이동(114)

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,23 +1,42 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 
+// 포탈 지정
+const Backdrop = ({ onConfirm }) => {
+  return <div className={classes.backdrop} onClick={onConfirm} />;
+};
+const ModalOverlay = ({ title, message, onConfirm }) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = ({ title, message, onConfirm }) => {
   return (
     <>
-      <div className={classes.backdrop} onClick={onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={onConfirm}>Okay</Button>
-        </footer>
-      </Card>
+      {/* ReactDOM.createPortal의 2가지 인자 : 렌더링 되어야하는 node(JSX로!),
+      포인터(렌더링 되어야하는 node 요소가 렌더링 되는 실제 DOM 컨테이너) */}
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
