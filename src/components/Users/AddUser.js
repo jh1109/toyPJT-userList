0ed5ae0283527cd577f29a40e0a45e64a6a10de2
Juhee Noh <nohjuhee227@gmail.com>
2023-06-08 22:22:44 +0900

    learn. 유효성 검사에 따라 모달창 띄우기(107)

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,12 +9,23 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = ({ onAddUser }) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
   const addUserHandler = (e) => {
     e.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
-    if (+enteredAge < 1) return;
+    if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
+      return;
+    }
     onAddUser(enteredUsername, enteredAge);
     setEnteredUsername("");
     setEnteredAge("");
@@ -27,7 +38,7 @@ const AddUser = ({ onAddUser }) => {
   };
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {error && <ErrorModal title={error.title} message={error.message} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
